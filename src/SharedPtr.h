5b12e733dda1e9743c99eac9cfb14eed5d18e1c0
deroij <deroij@ualberta.ca>
2026-09-15 18:24:43 -0600

    lab3 completed

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -3,10 +3,12 @@
 
 class ControlBlockBase {
 public:
-    ControlBlockBase(); // TODO: implement the default constructor.
+    ControlBlockBase(){
+    }; // TODO: implement the default constructor.
 
     // dtor is virtual, so that we can call derived class's dtor from a ptr to this base class.
-    virtual ~ControlBlockBase(); // TODO: implement the destructor.
+    virtual ~ControlBlockBase(){ //had delete here, but I believe that caused a double free
+    }; // TODO: implement the destructor.
 
     // pure virtual function; must be overriden by derived classes
     virtual void* managedAddress() = 0;
@@ -18,20 +20,166 @@ public:
     long increment()
     {
         // TODO: increment refcount by 1 and return result.
+        refcount++;
+        return refcount;
     }
 
     long decrement()
     {
         // TODO: decrement refcount by 1 and return result.
+        refcount--;
+        return refcount;
     }
 
     long refCount() const
     {
         // TODO: just return the refcount.
+        return refcount;
     }
 
 private:
     // TODO: add field(s) which both control block types need to have
+    long refcount = 0;
+    
 };
 
+template<typename T> class ControlBlock : public ControlBlockBase {
+public:
+    ControlBlock(T* ptr) : managedPtr(ptr) {
+    }
+
+    ~ControlBlock() override {
+        delete managedPtr;
+    }
+
+    void* managedAddress() override {
+        return managedPtr;
+    }
+
+private:
+    T* managedPtr;
+};
+
+template<typename T> class SharedPtr {
+    public:
+    //Constructors
+        SharedPtr() : ptr(nullptr), controlBlock(nullptr) {};
+        SharedPtr(T* rawPtr) : ptr(rawPtr), controlBlock(new ControlBlock<T>(rawPtr)) {controlBlock->increment();}; //Add a new count
+    //Destructor
+        ~SharedPtr() {
+            if (controlBlock) { //If control block
+                controlBlock->decrement(); //Say I'm leaving
+                if (controlBlock->refCount() == 0) { //If no more, delete the control block
+                    delete controlBlock;
+                }
+            }
+        }
+    //Copy
+        SharedPtr(const SharedPtr& newPtr) : ptr(newPtr.ptr), controlBlock(newPtr.controlBlock) {
+            if (controlBlock) {
+                controlBlock->increment();
+            }
+        }
+    //Copy assignment
+        SharedPtr& operator=(const SharedPtr& newPtr) {
+            if (this != &newPtr) { //Make sure we're not already copied
+                if (controlBlock) { //Get rid of old control block
+                    controlBlock->decrement();
+                    if (controlBlock->refCount() == 0) {
+                        delete controlBlock;
+                    }
+                }
+                ptr = newPtr.ptr; //Change to new pointer
+                controlBlock = newPtr.controlBlock;
+                if (controlBlock) {
+                    controlBlock->increment();
+                }
+            }
+            return *this;
+        }
+    //Move
+        SharedPtr(SharedPtr&& newPtr) : ptr(newPtr.ptr), controlBlock(newPtr.controlBlock) {
+            newPtr.ptr = nullptr; //Reset current pointer
+            newPtr.controlBlock = nullptr;
+        }
+    //Move assignment
+        SharedPtr& operator=(SharedPtr&& newPtr) {
+            if (this != &newPtr) { //Make sure we're not the same
+                if (controlBlock) { //Get rid of old control block
+                    controlBlock->decrement();
+                    if (controlBlock->refCount() == 0) {
+                        delete controlBlock;
+                    }
+                }
+                ptr = newPtr.ptr; //Change to new pointer
+                controlBlock = newPtr.controlBlock;
+                newPtr.ptr = nullptr; //Reset old pointer
+                newPtr.controlBlock = nullptr;
+            }
+            return *this;
+        }
+    //Dereference operator
+        T& operator*() const {
+            return *ptr;
+        }
+    //T* Get
+        T* get() {
+            return ptr;
+        }
+    //Comparison operator (and !=?)
+        bool operator==(const SharedPtr& other) const {
+            return ptr == other.ptr; //Use the raw pointers as they shouldn't be different
+        }
+    //Boolean conversion
+        explicit operator bool() const { //From google, explicit operator bool() const is how you do boolean conversion
+            return ptr != nullptr;
+        }
+    //Void swap (I don't touch the refcounts?)
+        void swap(SharedPtr& other) {
+            std::swap(ptr, other.ptr);
+            std::swap(controlBlock, other.controlBlock);
+        }
+    //Void reset
+        void reset() {
+            if (controlBlock) { //Explained this one before, scroll up
+                controlBlock->decrement();
+                if (controlBlock->refCount() == 0) {
+                    delete controlBlock;
+                }
+            }
+            ptr = nullptr;
+            controlBlock = nullptr;
+        }
+        void reset(T* newPtr) {
+            if (ptr == newPtr) { //If we're resetting to the same pointer, don't do anything
+                return;
+            }
+
+            if (controlBlock) {
+                controlBlock->decrement();
+                if (controlBlock->refCount() == 0) {
+                    delete controlBlock;
+                }
+            }
+            ptr = newPtr;
+            controlBlock = new ControlBlock<T>(newPtr);
+            controlBlock->increment();
+        }
+    //Usecount
+        long useCount() const {
+            if (controlBlock) {
+                return controlBlock->refCount();
+            }
+            return 0;
+        }
+    
+    private:
+        T* ptr;
+        ControlBlockBase* controlBlock;
+};
+
+template <typename T, typename... Args> SharedPtr<T> makeSharedBasic(Args&&... args) {
+    T* rawPtr = new T(std::forward<Args>(args)...); //Make raw pointer pointing to new T object with forwarded args
+    return SharedPtr<T>(rawPtr); //Convert raw pointer to shared pointer
+}
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,74 @@
 #include <cstdlib>
+#include <iostream> //Added for cout
 #include "SharedPtr.h"
 
 int main() {
+   std::cout << "Test 1a, default construct, bool, get, equal, copy assign. Goal: 0 5 false 5 true\n";
+   SharedPtr<int> a;
+   if (a) {
+      std::cout << *a.get() << " ";
+   } else {
+      std::cout << 0 << " ";
+   }
+   SharedPtr<int> b(new int(5));
+   std::cout << *b.get() << " ";
+   std::cout << (a == b?"true":"false") << " ";
+   a = b;
+   std::cout << *a.get() << " ";
+   std::cout << (a == b?"true":"false") << "\n";
+
+   std::cout << "Test 1b, move construct, get, equal. Goal: 5 true\n";
+   SharedPtr<int> c;
+   c = std::move(b);
+   std::cout << *c.get() << " ";
+   std::cout << (c == b?"true":"false") << "\n";
+
+   std::cout << "Test 1c, copy construct, get, equal, useCount. Goal: 5 true 3\n";
+   SharedPtr<int> d(a);
+   std::cout << *d.get() << " ";
+   std::cout << (d == a?"true":"false") << " ";
+   std::cout << d.useCount() << "\n";
+
+   std::cout << "Test 1d, reset, get, equal, useCount. Goal: 0 false 2\n";
+   d.reset();
+   if (d) {
+      std::cout << *d.get() << " ";
+   } else {
+      std::cout << 0 << " ";
+   }
+   std::cout << (d == a?"true":"false") << " ";
+   std::cout << d.useCount() << "\n";
+
+   std::cout << "Test 1e, reset with new pointer, get, equal, useCount. Goal: 2 false 1\n";
+   d.reset(new int(2));
+   std::cout << *d.get() << " ";
+   std::cout << (d == a?"true":"false") << " ";
+   std::cout << d.useCount() << "\n";
+
+   std::cout << "Test 1f, selfreset, get, useCount. Goal: 2 1\n";
+   d.reset(d.get());
+   std::cout << *d.get() << " ";
+   std::cout << d.useCount() << "\n";
+
+   std::cout << "Test 1g, swap, get, equal, useCount. Goal: 5 2 false 2 1\n";
+   d.swap(a);
+   std::cout << *d.get() << " ";
+   std::cout << *a.get() << " ";
+   std::cout << (d == a?"true":"false") << " ";
+   std::cout << d.useCount() << " ";
+   std::cout << a.useCount() << "\n";
+
+   std::cout << "Test 1h, scope test, copy, useCount. Goal: 2 2 1\n";
+   {
+      SharedPtr<int> e(a);
+      std::cout << *e.get() << " ";
+      std::cout << a.useCount() << " ";
+   }
+   std::cout << a.useCount() << "\n";
+
+   std::cout << "Final test, makeSharedBasic, get Goal: 5\n";
+   SharedPtr<int> five = makeSharedBasic<int>(5);
+   std::cout << *five.get() << "\n";
+
    return EXIT_SUCCESS;
 }
